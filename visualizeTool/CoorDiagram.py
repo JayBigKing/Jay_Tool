f3f2204bba79996dbed01c89219b3cfada62622a
jay.zhu <873442494@qq.com>
2022-11-02 16:21:31 +0800

    NEW: 1. 为平面绘图添加了x,y标签；2. x数轴显示优化；3. 添加图例功能；4. 添加了保存figure的功能
    WHAT: 和NEW一样
    
    WHY: 1. 原始的平面绘图可视化还是有点简陋的
    
    TEST: pass

--- visualizeTool/CoorDiagram.py
+++ visualizeTool/CoorDiagram.py
@@ -1,32 +1,40 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+
 
 class CoorDiagram:
-    def __init__(self):
-        pass
-    def drawOneLine(self,x,y):
-        plt.plot(x,y)
+    def __init__(self, storePath = "./"):
+        self.storePath = storePath
+        print(os.getcwd())
+    def setStorePath(self, storePath):
+        self.storePath = storePath
+
+    def drawOneLine(self, x, y):
+        plt.plot(x, y)
         plt.show()
-    def drawManyLines(self,xList,yList):
+
+    def drawManyLines(self, xList, yList):
         if xList is None or yList is None:
             return
-        for i in range(0,len(xList)):
+        for i in range(0, len(xList)):
             x = xList[i]
             y = yList[i]
             plt.figure()
             plt.plot(x, y)
         plt.show()
 
-    def drawOneScatter(self,scatter):
+    def drawOneScatter(self, scatter):
         # plt.figure(figsize=(10, 10), dpi=100)
         scatter = np.array(scatter)
         x = scatter[:, 0]
         y = scatter[:, 1]
         plt.figure()
-        plt.scatter(x,y)
+        plt.scatter(x, y)
         plt.show()
 
-    def drwaManyScatters(self,scattersList,colorList = None):
+    def drawManyScatters(self, scattersList, colorList=None):
         if scattersList is None:
             return
         for scatter in scattersList:
@@ -35,33 +43,53 @@ class CoorDiagram:
             y = scatter[:, 1]
             plt.figure()
             if colorList is not None:
-                plt.scatter(x, y,c=colorList[i])
-                plt.plot(x,y)
+                plt.scatter(x, y, c=colorList[i])
+                plt.plot(x, y)
             else:
                 plt.scatter(x, y)
                 plt.plot(x, y)
         plt.show()
 
-    def drwaManyScattersInOnePlane(self,scattersList, nameList = None, theS = 30):
-        if scattersList is None :
+    def drawManyScattersInOnePlane(self, scattersList, nameList=None, labelNames=None, titleName=None, ifSaveFig=False,
+                                   saveFigName=None):
+        if scattersList is None:
             return
-        for scatter in scattersList:
-            scatter = np.array(scatter)
-            minVal = np.min(scatter[:, 0])
-            maxVal = np.max(scatter[:, 0])
+        if labelNames is None:
+            labelNames = ["x", "y"]
 
-        # theX = range(int(minVal - 1),int(maxVal + 1))
         for index, scatter in enumerate(scattersList):
             scatter = np.array(scatter)
             x = scatter[:, 0]
             y = scatter[:, 1]
-            # plt.figure()
 
             plt.scatter(x, y)
-            plt.plot(x, y)
+
             if nameList is None:
-                plt.text(x[0],y[0],"orgin",ha='center',va='bottom')
+                plt.plot(x, y)
+                plt.text(x[0], y[0], "orgin", ha='center', va='bottom')
             else:
-                plt.text(x[0], y[0], r"%s orgin"%nameList[index], ha='center', va='bottom')
-        # plt.xticks(theX)
+                plt.plot(x, y, label = nameList[index])
+                plt.text(x[0], y[0], r"%s orgin" % nameList[index], ha='center', va='bottom')
+                plt.legend()
+
+        if titleName is not None:
+            plt.title(titleName)
+
+        plt.xlabel(labelNames[0])
+        plt.ylabel(labelNames[1])
+
+        self.saveFigure(ifSaveFig, saveFigName)
         plt.show()
+
+    def figureFileNameGenerate(self):
+        return time.strftime("figure%Y%m%d_%H%M%S", time.localtime())
+
+    def saveFigure(self, ifSaveFig, saveFigName):
+        if ifSaveFig is True:
+            if saveFigName is None:
+                saveFigName = self.figureFileNameGenerate()
+            if os.path.exists(self.storePath) is False:
+                os.mkdir(self.storePath)
+
+            plt.savefig("{:}{:}".format(self.storePath, saveFigName))
+
