95738b1a3db1c931a77c185eaf3a0da9f7205c02
jay.zhu <873442494@qq.com>
2023-07-30 17:05:54 +0800

    1. 通过关闭OMP，优化了多进程的效率； 2. 添加了有一个中心节点进行优化的对比实验

--- EfficiencyTestTool/EfficiencyTestTool.py
+++ EfficiencyTestTool/EfficiencyTestTool.py
@@ -2,7 +2,8 @@ import time
 import functools
 from Jay_Tool.LogTool import myLogger
 
-def clockTester(func):
+
+def clockTester(func, printRes=False, printArgs=False):
     @functools.wraps(func)
     def clocked(*args, **kwargs):
         t0 = time.time()
@@ -10,16 +11,24 @@ def clockTester(func):
         elapsed = time.time() - t0
         name = func.__name__
         arg_lst = []
-        if args:
-            arg_lst.append(', '.join(repr(arg) for arg in args))
-        if kwargs:
-            pairs = ['%s=%r' % (k,w) for k, w in sorted(kwargs.items())]
-            arg_lst.append(', '.join(pairs))
+        if printArgs is True:
+            if args:
+                arg_lst.append(', '.join(repr(arg) for arg in args))
+            if kwargs:
+                pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+                arg_lst.append(', '.join(pairs))
         arg_str = ', '.join(arg_lst)
-        if myLogger.consoleLogger is None:
-            print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+
+        if printRes is False:
+            if myLogger.consoleLogger is None:
+                print('[%0.8fs] %s(%s)' % (elapsed, name, arg_str))
+            else:
+                myLogger.myLogger_Logger().info('[%0.8fs] %s(%s)' % (elapsed, name, arg_str))
         else:
-            myLogger.myLogger_Logger().info('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+            if myLogger.consoleLogger is None:
+                print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+            else:
+                myLogger.myLogger_Logger().info('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
 
         return result
 
